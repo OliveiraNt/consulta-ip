8415fb0948a464ef646645c394f539537033a16b
SCN0519 <solonneto.lumis@sulamerica.com.br>
2019-11-22 13:03:43 -0300

    logs

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  const ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
-  res.send(ip);
+  const header = req.header('x-forwarded-for');
+  const remoteAddress = req.connection.remoteAddress;
+  console.log(`header: ${header}`);
+  console.log(`remoteAddress: ${remoteAddress}`);
+  const ip = header || remoteAddress;
+  console.log(`IP: ${ip}`);
+  res.send({ip});
 });
 
 app.listen(process.env.PORT || 8080, () => console.log('consulta-ip executando na porta 8080 \n\n'));
